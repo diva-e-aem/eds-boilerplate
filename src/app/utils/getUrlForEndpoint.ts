8b215ac8a1e7be70d2a8a8edc0740fe3823c0028
Ingo Fahrentholz <ingo.fahrentholz@mb-2244.localdomain>
2024-06-21 14:50:14 +0200

    fix getUrlForEndpoint on iframe (used in sidekick library)

--- src/app/utils/getUrlForEndpoint.ts
+++ src/app/utils/getUrlForEndpoint.ts
@@ -1,13 +1,27 @@
+import { getOrigin } from 'Helpers/sidekick/getOrigin';
+
 /**
- * Get the URL for an endpoint.
- * @param {string} endpoint - The endpoint.
- * @returns {URL}
+ * Constructs a complete URL for a given endpoint relative to the base URL.
+ *
+ * This function takes an endpoint as input and constructs a full URL using the
+ * base URL derived from `window.hlx.codeBasePath` and the origin of the current
+ * location. If the URL construction fails, an error is thrown.
+ *
+ * @param {string} endpoint - The endpoint to construct the URL for.
+ * @returns {URL} - The constructed URL object.
+ * @throws {Error} - Throws an error if the URL construction fails.
+ *
  * @example
- * const url = getUrlForEndpoint('block1/block1.css');
- * console.log(url);
- * Output: URL { href: 'http://localhost:3000/block1/block1.css' }
+ * // Assuming window.hlx.codeBasePath is '/base/path/' and the origin is 'https://example.com'
+ * const url = getUrlForEndpoint('/api/data');
+ * console.log(url.toString()); // Output: 'https://example.com/base/path/api/data'
  */
-export const getUrlForEndpoint = (endpoint: string): URL => {
-  const baseUrl = new URL(window.hlx.codeBasePath, window.location.origin);
-  return new URL(endpoint, baseUrl);
+export const getUrlForEndpoint = (endpoint: string): URL | never => {
+  try {
+    const location = getOrigin();
+    const baseUrl = new URL(window.hlx.codeBasePath, location);
+    return new URL(endpoint, baseUrl);
+  } catch (error) {
+    throw new Error(`Failed to build Url for endpoint: ${error}`);
+  }
 };
