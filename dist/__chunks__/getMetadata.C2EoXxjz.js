506a242390f5df2a12da20a0de2cdd2ebd3c213a
samuelecalamita <samuelecalamita@gmail.com>
2024-08-29 10:02:50 +0200

    remove dist

--- dist/__chunks__/getMetadata.C2EoXxjz.js
+++
@@ -1,2 +0,0 @@
-function r(t,n=document){const a=t&&t.includes(":")?"property":"name",e=[...n.head.querySelectorAll(`meta[${a}="${t}"]`)].map(o=>o.content).join(", ");return e.length?e:""}export{r as g};
-//# sourceMappingURL=getMetadata.C2EoXxjz.js.map
